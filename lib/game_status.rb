f766da355331ce7712ab4c6690332fd97eb7cc44
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-20 23:07:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,7 +1,26 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
-def win_combinations
+WIN_COMBINATIONS = [
+  [0,1,2]
+  [0,4,8]
+  [0,3,6]
+  [1,4,7]
+  [2,4,6]
+  [2,5,8]
+  [3,4,5]
+  [6,7,8]
+]
+
