2c507c436bfc9827ee2ee1319147cc9387dde563
Hunter Sapienza <sapienzahunter@gmail.com>
2018-05-20 23:22:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,10 +35,10 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination.to_ary
+      return win_combination
       winner = "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination.to_ary
+      return win_combination
       winner = "O"
     else
       return false
